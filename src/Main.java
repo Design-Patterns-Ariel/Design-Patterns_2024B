5f118e948931d682da5795b683c944ee4d8272ca
Barak sharbi <41991103+bsharabi@users.noreply.github.com>
2024-04-09 14:27:55 +0300

    org

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import Singleton.Java.Singleton;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        // db = new db("")
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Singleton.getInstance());
+        }
+        System.out.println("-----------------------------------");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Singleton.egetInstance());
+        }
+    }
+}
